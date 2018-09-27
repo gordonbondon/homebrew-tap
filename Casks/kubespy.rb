cc8f0219c9cf8ddb6245e1c221a6db850ced999c
Artem Iarmoliuk <koolgen@gmail.com>
2018-09-27 16:53:07 +0300

    Fix cask name

--- Casks/kubespy.rb
+++ Casks/kubespy.rb
@@ -3,7 +3,7 @@ cask 'kubespy' do
   sha256 'ff80b99aacb401c5370fc816568b9f9ff4a927c2d6b6a8f5b9d3a809b800cee8'
 
   url "https://github.com/pulumi/kubespy/releases/download/v#{version}/kubespy_darwin-amd64"
-  name 'zms-cli'
+  name 'kubespy'
   homepage 'https://github.com/pulumi/kubespy'
 
   binary 'kubespy_darwin-amd64', target: 'kubespy'
