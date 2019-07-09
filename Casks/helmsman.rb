e4ea7689e02097c3d5333bc8044396407cc27a7f
Artem Yarmoliuk <koolgen@gmail.com>
2019-07-09 13:56:54 +0300

    helmsman-1.10.0

--- Casks/helmsman.rb
+++ Casks/helmsman.rb
@@ -1,6 +1,6 @@
 cask 'helmsman' do
-  version '1.8.1'
-  sha256 'fd2d2c750960ba331ee99f590db2f65f8a075872754480a1768c95587f8cbee4'
+  version '1.10.0'
+  sha256 '06061170acc69f26785aac073a4e9da2a05de9699d69f6d34f775ef4299ba3f1'
 
   url "https://github.com/Praqma/helmsman/releases/download/v#{version}/Helmsman_#{version}_darwin_amd64.tar.gz"
   name 'helmsman'
