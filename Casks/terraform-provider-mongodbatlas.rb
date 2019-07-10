580d137e04773afd296f09527513aa501dde7c9a
Artem Yarmoliuk <koolgen@gmail.com>
2019-07-10 13:31:50 +0300

    mongodbatlas-1.1.1

--- Casks/terraform-provider-mongodbatlas.rb
+++ Casks/terraform-provider-mongodbatlas.rb
@@ -1,10 +1,10 @@
 cask 'terraform-provider-mongodbatlas' do
-  version '1.1.0'
-  sha256 '7589a1b0c7d7fbe5e842326f978e5c8bffa0b6b908f6b17f8fdbe5a2d7c4568d'
+  version '1.1.1'
+  sha256 '6197a07b7b9f1aa7e05d2be382ea7bbd2dce9e43c35e40db8ab65282a921bb2e'
 
-  url "https://github.com/akshaykarle/terraform-provider-mongodbatlas/releases/download/v#{version}/terraform-provider-mongodbatlas_v#{version}_darwin_amd64"
+  url "https://github.com/koalificationio/terraform-provider-mongodbatlas/releases/download/v#{version}-rc/terraform-provider-mongodbatlas_v#{version}_darwin_amd64"
   name 'terraform-provider-mongodbatlas'
-  homepage 'https://github.com/akshaykarle/terraform-provider-mongodbatlas'
+  homepage 'https://github.com/koalificationio/terraform-provider-mongodbatlas'
 
   depends_on formula: 'gordonbondon/tap/tf-install-provider'
 
